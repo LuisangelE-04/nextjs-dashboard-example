00e838f210606e4a02323b34cf86b9034dcc9c70
LuisangelE-04 <luisangel.esparza2022@gmail.com>
2025-07-22 16:52:17 -0500

    feat(invoice): add feedback to edit invoice page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -64,13 +64,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validateFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validateFields.data;
   const amountInCents = amount * 100;
 
   try {
@@ -80,7 +88,7 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
     `;
   } catch (error) {
-    console.log(error);
+    return { message: 'Database Error: Failed to Update.' };
   }
 
   revalidatePath('dashboard/invoices');
