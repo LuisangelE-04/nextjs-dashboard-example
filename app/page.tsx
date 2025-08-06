2b24a14fe3e20eda42525e68157f6ad9880ba0e9
LuisangelE-04 <luisangel.esparza2022@gmail.com>
2025-08-06 12:38:26 -0500

    feat(login): add login verification

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
-import Image from 'next/image';``
+import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Welcome to Acme',
+};
 
 export default function Page() {
   return (
